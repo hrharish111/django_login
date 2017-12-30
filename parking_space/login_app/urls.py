fa91af84069e2305dab855a0d5c9549860bc97e7
hrharish111 <Rudresh@123>
2017-12-30 16:04:35 +0530

    project submit

--- parking_space/login_app/urls.py
+++ parking_space/login_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^$',auth_views.login,kwargs = {'template_name':'login_app/home.html', "redirect_authenticated_user":True},name="login"),
+    url(r'^slot_viewer/$',views.slot_viewer,name='slot_viewer'),
     url(r'^logout/$',auth_views.logout,{'template_name':'login_app/logout.html',},name="logout")
 ]
