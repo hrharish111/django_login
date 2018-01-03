9de01a1bfdf64c01e3d6c6e1d17d8b800dee4d68
hrharish111 <Rudresh@123>
2018-01-03 21:48:40 +0530

    login_with_slotbookwithsinglefile

---
+++ parking_space/runstandalone.py
@@ -0,0 +1,13 @@
+import os
+import pip
+from subprocess import call
+def install(package):
+    pip.main(["install","--upgrade","--no-index","--find-links=.",package])
+
+if __name__=="__main__":
+    avaliable_packages = sorted(["%s==%s" %(i.key,i.version)for i in pip.get_installed_distributions()])
+
+    if "django==1.11" not in avaliable_packages:
+        install("Django-1.11-py2.py3-none-any.whl")
+
+    call(["python","manage.py","runserver"])
